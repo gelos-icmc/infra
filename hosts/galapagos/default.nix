c7f2b4b02eb3156d8d574a042810c5844ea13cd1
Gabriel Fontes <eu@misterio.me>
2022-05-11 03:15:59 -0300

    adicionar config completa do galapagos

--- hosts/galapagos/default.nix
+++ hosts/galapagos/default.nix
@@ -8,19 +8,32 @@
   boot = {
     # Kernel
     kernelPackages = pkgs.linuxPackages_zen;
-    # Plymouth (currently only starts at phase 2)
-    plymouth = {
-      enable = true;
-    };
-    # Bootloader configuration
     loader = {
-      timeout = 0;
-      systemd-boot = {
-        enable = true;
-        consoleMode = "max";
-        editor = false;
-      };
+      systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking = {
+    hostName = "galapagos";
+    nameservers = [ "143.107.253.3" ];
+    interfaces = {
+      # Interface WAN
+      # Conectada a internet da USP, IP estatico
+      eno1 = {
+        useDHCP = false;
+        ipv4 = {
+          addresses = [{
+            address = "143.107.183.251";
+            prefixLength = 26;
+          }];
+          routes = [{
+            address = "0.0.0.0";
+            prefixLength = 0;
+            via = "143.107.183.193";
+          }];
+        };
+      };
     };
   };
 }
