5db9c5fef1f1a76e8a0b10f5bf1509585b6b5066
Gabriel Fontes <eu@misterio.me>
2022-05-11 05:41:35 -0300

    completar config do rockhopper

--- hosts/rockhopper/hardware-configuration.nix
+++ hosts/rockhopper/hardware-configuration.nix
@@ -1,6 +1,15 @@
 { modulesPath, hostname, ... }: {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
+  boot = {
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "sdhci_pci" "rtsx_pci_sdmmc" ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
+
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/${hostname}";
