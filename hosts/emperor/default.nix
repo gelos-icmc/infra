cd7fdb6d46b3dd43099445ffd49df4dbd2606a9d
Gabriel Fontes <eu@misterio.me>
2022-05-11 01:19:26 -0300

    correcao do IP, endurecer SSH e hospedar em porta 2112

--- hosts/emperor/default.nix
+++ hosts/emperor/default.nix
@@ -18,13 +18,13 @@
     hostName = "emperor";
     nameservers = [ "143.107.253.3" ];
     interfaces = {
-      # Interface WAN 
+      # Interface WAN
       # Conectada a internet da USP, IP estatico
       eno1 = {
         useDHCP = false;
         ipv4 = {
           addresses = [{
-            address = "143.107.183.251";
+            address = "143.107.183.252";
             prefixLength = 26;
           }];
           routes = [{
