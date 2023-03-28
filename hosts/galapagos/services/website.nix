e5fc97da5c41c94bdb6300de17b11e31175880f3
Gabriel Fontes <hi@m7.rs>
2023-03-28 15:28:55 -0300

    site: simplificar cache

--- hosts/galapagos/services/website.nix
+++ hosts/galapagos/services/website.nix
@@ -1,13 +1,7 @@
 { inputs, pkgs, ... }:
 let
   mainPkg = flake: flake.packages.${pkgs.system}.default;
-  lastModified = flake: convertDateTime flake.lastModified;
-  convertDateTime = timestamp: builtins.readFile (
-    pkgs.runCommandLocal "datetime" { } ''
-      dt="$(date -Ru -d @${toString timestamp})"
-      echo -n ''${dt/+0000/GMT} > $out
-    ''
-  );
+  days = n: toString (n * 60 * 60 * 24);
 in
 {
   services.nginx.virtualHosts."gelos.club" = {
@@ -16,23 +10,19 @@ in
     enableACME = true;
     locations = {
       "/" = {
-        alias = "${mainPkg inputs.gelos-site}/public/";
-        # Adicionar header indicando data de modificação
-        # Pra permitir que o navegador cacheie
+        root = "${mainPkg inputs.gelos-site}/public";
+      };
+      "/assets/" = {
+        root  = "${mainPkg inputs.gelos-site}/public";
         extraConfig = ''
-          add_header Last-Modified "${lastModified inputs.gelos-site}";
+          add_header Cache-Control "max-age=${days 1}, stale-while-revalidate=${days 30}";
         '';
       };
       "=/identidade" = {
         return = "301 https://gelos.club/identidade/";
       };
       "/identidade/" = {
-        alias = "${mainPkg inputs.gelos-identidade-visual}/";
-        # Adicionar header indicando data de modificação
-        # Pra permitir que o navegador cacheie
-        extraConfig = ''
-          add_header Last-Modified "${lastModified inputs.gelos-identidade-visual}";
-        '';
+        root = "${mainPkg inputs.gelos-identidade-visual}/";
       };
     };
   };
