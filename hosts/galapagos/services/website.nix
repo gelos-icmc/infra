ded4be8c1b2697f3b5812f96cf6a2f7df37354a8
Gabriel Fontes <hi@m7.rs>
2023-08-14 10:27:59 -0300

    adicionar redirecionamentos para os *.gelos.icmc.usp.br

--- hosts/galapagos/services/website.nix
+++ hosts/galapagos/services/website.nix
@@ -7,32 +7,44 @@
   minutes = n: toString (n * 60);
   days = n: toString (n * 60 * 60 * 24);
 in {
-  services.nginx.virtualHosts."gelos.club" = {
-    default = true;
-    forceSSL = true;
-    enableACME = true;
-    locations = {
-      "/" = {
-        root = "${mainPkg inputs.gelos-site}/public";
-        extraConfig = ''
-          add_header Cache-Control "stale-while-revalidate=${minutes 5}";
-        '';
-      };
-      "/assets/" = {
-        root = "${mainPkg inputs.gelos-site}/public";
-        extraConfig = ''
-          add_header Cache-Control "max-age=${days 1}, stale-while-revalidate=${days 30}";
-        '';
-      };
-      "=/identidade" = {
-        return = "301 https://gelos.club/identidade/";
-      };
-      "/identidade/" = {
-        alias = "${mainPkg inputs.gelos-identidade-visual}/";
-      };
+  services.nginx.virtualHosts = {
+    "gelos.club" = {
+      default = true;
+      forceSSL = true;
+      enableACME = true;
+      locations = {
+        "/" = {
+          root = "${mainPkg inputs.gelos-site}/public";
+          extraConfig = ''
+            add_header Cache-Control "stale-while-revalidate=${minutes 5}";
+          '';
+        };
+        "/assets/" = {
+          root = "${mainPkg inputs.gelos-site}/public";
+          extraConfig = ''
+            add_header Cache-Control "max-age=${days 1}, stale-while-revalidate=${days 30}";
+          '';
+        };
+        "=/identidade" = {
+          return = "301 https://gelos.club/identidade/";
+        };
+        "/identidade/" = {
+          alias = "${mainPkg inputs.gelos-identidade-visual}/";
+        };
 
-      # Permalinks mais curtinhos, geralmente usados pra QRs
-      "=/debian-day".return = "301 https://gelos.club/2023/08/02/debian-day.html";
+        # Permalinks mais curtinhos, geralmente usados pra QRs
+        "=/debian-day".return = "301 https://gelos.club/2023/08/02/debian-day.html";
+      };
+    };
+    "gelos.icmc.usp.br" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/".return = "302 https://gelos.club$request_uri";
+    };
+    "galapagos.gelos.icmc.usp.br" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/".return = "302 https://gelos.club$request_uri";
     };
   };
 }
