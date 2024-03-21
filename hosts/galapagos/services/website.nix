2bc498d6fa2b50f1aff5d6293180d3e8858c10db
Gabriel Fontes <hi@m7.rs>
2024-03-21 11:46:42 -0300

    stale-while-revalidate não funciona sem max-age

--- hosts/galapagos/services/website.nix
+++ hosts/galapagos/services/website.nix
@@ -17,7 +17,7 @@ in {
         "/" = {
           root = "${mainPkg inputs.gelos-site}/public";
           extraConfig = ''
-            add_header Cache-Control "stale-while-revalidate=${minutes 60}";
+            add_header Cache-Control "max-age="${minutes 1}, stale-while-revalidate=${minutes 60}";
             # Antigo link de atas
             rewrite ^/([0-9]+)/([0-9]+)/([0-9]+)/ata\.html$ /reunioes/$1-$2-$3.html permanent;
           '';
