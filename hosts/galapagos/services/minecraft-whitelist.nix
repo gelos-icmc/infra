8e2922d398a5641cd26812fe6e7132641003fbaa
natanbc <natanbc@users.noreply.github.com>
2024-12-22 17:50:19 +0000

    Fix nix syntax on whitelist

--- hosts/galapagos/services/minecraft-whitelist.nix
+++ hosts/galapagos/services/minecraft-whitelist.nix
@@ -5,5 +5,5 @@
   natanbc = "7ba397ae-990e-4619-b058-2f669f0be024";
   figurantplusplus = "7862f7c3-9350-4f9d-b5d6-1a25aee78b48";
   EpicRon = "70d4caad-fb6a-487a-a4f9-6bbbcfba22f7";
-  1Storce1 = "3d5bafe4-dc52-4b6e-9e0e-7cb989dfa9c9";
+  "1Storce1" = "3d5bafe4-dc52-4b6e-9e0e-7cb989dfa9c9";
 }
