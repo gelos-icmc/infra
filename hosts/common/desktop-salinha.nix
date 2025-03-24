bc1c917e2a55bb559a98bf6022e9a170dfc568f5
GELOS Entity <contato@gelos.club>
2025-03-24 17:55:23 -0300

    Impressora

--- hosts/common/desktop-salinha.nix
+++ hosts/common/desktop-salinha.nix
@@ -82,6 +82,11 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
